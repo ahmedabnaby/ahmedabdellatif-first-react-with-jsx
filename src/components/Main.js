dbd3140f22b8f37e180575a7695e9b9f4c326f4a
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2022-08-16 18:16:36 +0200

    Assignment ready

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Main = ()=>{
+    return(
+       <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Marathon</li> 
+        <li>Feed the dogs.</li> 
+       </ul>
+    )
+}
+
+export default Main;
